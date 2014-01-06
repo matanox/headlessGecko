de7f4e910f1f9a2a770693b354d86471236487e2
matanster <dev.matan@gmail.com>
2014-01-06 16:35:19 +0200

    initial commit

---
+++ example.js
@@ -0,0 +1,14 @@
+var webpageModule = require("webpage");
+
+var page = require("webpage").create();
+page.open("http://slimerjs.org")
+    .then(function(status){
+         if (status == "success") {
+             console.log("The title of the page is: "+ page.title);
+         }
+         else {
+             console.log("Sorry, the page is not loaded");
+         }
+         page.close();
+         phantom.exit();
+    })
---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install xvfb
+curl -O http://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/25.0/runtimes/xulrunner-25.0.en-US.linux-x86_64.tar.bz2
+tar -xvf xulrunner-25.0.en-US.linux-x86_64.tar.bz2
+curl -O http://download.slimerjs.org/v0.9/0.9.0/slimerjs-0.9.0.zip
+unzip slimerjs-0.9.0.zip
